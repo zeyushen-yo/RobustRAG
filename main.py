2250160d53db76c2a7bd672e3ee941fad68cdfad
bi0600@della8.princeton.edu <imanabasileal@gmail.com>
2025-04-16 14:55:59 -0400

    add togetherai and count tokens

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ from src.attack import *
 from src.helper import get_log_name
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+import csv
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Robust RAG')
 
     # LLM settings
     parser.add_argument("--local_rank", type=int, default=0, help="Local rank passed from distributed launcher")
-    parser.add_argument('--model_name', type=str, default='mistral7b',choices=['mistral7b', 'llama3b', 'gpt-4o', 'o1-mini', 'deepseek7b', 'llama1b'],help='model name')
+    parser.add_argument('--model_name', type=str, default='mistral7b',choices=['mistral7b', 'llama3b', 'gpt-4o', 'o1-mini', 'deepseek7b', 'llama1b', 'tai_llama8b','tai_mistral7b'],help='model name')
     parser.add_argument('--dataset_name', type=str, default='realtimeqa',choices=['realtimeqa-mc','realtimeqa','open_nq','biogen', 'simpleqa'],help='dataset name')
+    parser.add_argument('--model_dir', type=str, help='directory for huggingface models')
     parser.add_argument('--rep', type=int, default=1, help='number of times to repeat querying')
     parser.add_argument('--top_k', type=int, default=10,help='top k retrieval')
 
@@ -43,10 +46,11 @@ def parse_args():
     parser.add_argument('--save_response', action = 'store_true', help='save the results for later analysis')
     parser.add_argument('--use_cache', action = 'store_true', help='save/use cache responses from LLM')
     parser.add_argument('--no_vanilla', action = 'store_true', help='do not run vanilla RAG')
+    parser.add_argument('--max_samples', type=int, default=None, help='limit maximum number of samples to run for testing') 
+    parser.add_argument('--use_open_model_api', action = 'store_true', help='use local model instead of APIs for open models') 
 
     args = parser.parse_args()
     return args
-
 
 def main():
     args = parse_args()
@@ -76,10 +80,10 @@ def main():
         cache_path = None
 
     if args.dataset_name == 'biogen':
-        llm = create_model(args.model_name,max_output_tokens=500)
+        llm = create_model(args.model_name,args.model_dir, args.use_open_model_api, max_output_tokens=500)
         longgen = True
     else:
-        llm = create_model(args.model_name,cache_path=cache_path)
+        llm = create_model(args.model_name,args.model_dir, args.use_open_model_api, cache_path=cache_path)
         longgen = False
     no_defense = args.defense_method == 'none' or args.top_k<=0 # do not run defense
 
@@ -87,6 +91,25 @@ def main():
         gamma_values = [1] # dummy. gamma is not useful in this case
     else:
         gamma_values = [0.5, 0.8, 1.0]
+
+    output_csv_file = f"./output/{LOG_NAME}.csv"
+    fieldnames = [
+        "gamma",
+        "rank",
+        "undefended_acc",
+        "defended_acc",
+        "undefended_asr",
+        "defended_asr",
+        "undefended_input_tokens",
+        "undefended_output_tokens",
+        "defended_input_tokens",
+        "defended_output_tokens",
+        "undefended_total_time_sec",
+        "defended_total_time_sec"
+    ]
+    with open(output_csv_file, mode='w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
 
     results_all = []
     for gamma in gamma_values:
@@ -137,19 +160,39 @@ def main():
             undefended_corr_cnt = 0
             undefended_asr_cnt = 0
             defended_asr_cnt = 0
-            response_list = []
-            for data_idx, data_item in enumerate(tqdm(data_tool.data)):
+            #response_list = []
+            defended_input_tokens = 0
+            defended_output_tokens = 0
+            undefended_input_tokens = 0
+            undefended_output_tokens = 0
+
+            defended_total_time = 0
+            undefended_total_time = 0
+
+            data_list = data_tool.data
+            if args.max_samples is not None:
+                data_list = data_list[:args.max_samples]
+            for data_idx, data_item in enumerate(tqdm(data_list)):
                 data_item = data_tool.process_data_item(data_item)
                 if not no_attack:
                     data_item = attacker.attack(data_item)
                 
                 # undefended
                 if not args.no_vanilla:
+                    start_time = time.perf_counter()
+                    llm.reset_token_count()
                     for rep_idx in range(args.rep):
-                        logger.info(f'==== attackpos: {i}, item: {data_idx}, vanilla rep: {rep_idx}')
+                        logger.info(f'==== gamma: {gamma}, attackpos: {i}, item: {data_idx}, vanilla rep: {rep_idx}')
                         response_undefended = model.query_undefended(data_item)
                         undefended_corr = data_tool.eval_response(response_undefended,data_item)
                         undefended_corr_cnt += undefended_corr
+                    token_count = llm.get_token_count()
+                    undefended_input_tokens += token_count["input"]
+                    undefended_output_tokens += token_count["output"]
+                    llm.reset_token_count()
+
+                    end_time = time.perf_counter()
+                    undefended_total_time += (end_time - start_time)
                 else:
                     response_undefended = ''
                     undefended_corr = False
@@ -158,23 +201,33 @@ def main():
                     undefended_asr = data_tool.eval_response_asr(response_undefended,data_item)
                     undefended_asr_cnt += undefended_asr
                 
-                response_list.append({"query":data_item["question"], "undefended":response_undefended})
+                #response_list.append({"query":data_item["question"], "undefended":response_undefended})
                 
                 if not no_defense:
+                    start_time = time.perf_counter()
+                    llm.reset_token_count()
                     for rep_idx in range(args.rep):
-                        logger.info(f'==== attackpos: {i}, item: {data_idx}, defended rep: {rep_idx}')
+                        logger.info(f'==== gamma: {gamma}, attackpos: {i}, item: {data_idx}, defended rep: {rep_idx}')
                         if args.defense_method == "graph" or args.defense_method == "MIS":
                             response_defended = model.query(data_item)
                         elif args.defense_method in ['astuterag', 'instructrag_icl']:
                             response_defended = model.query(data_item)
-                        else:              
+                        else:
                             response_defended = model.query(data_item, gamma=gamma)
+                        
                         defended_corr = data_tool.eval_response(response_defended,data_item)
                         defended_corr_cnt += defended_corr
+                        
                         if not no_attack:
                             defended_asr = data_tool.eval_response_asr(response_defended,data_item)
                             defended_asr_cnt += defended_asr
-                        response_list.append({"query":data_item["question"],"defended":response_defended})
+                        #response_list.append({"query":data_item["question"],"defended":response_defended})
+                    token_count = llm.get_token_count()
+                    defended_input_tokens += token_count["input"]
+                    defended_output_tokens += token_count["output"]
+                    llm.reset_token_count()
+                    end_time = time.perf_counter()
+                    defended_total_time += (end_time - start_time)
 
             logger.info(f'Params: Gamma: {gamma}, rank: {i}')
             logger.info(f'undefended_corr_cnt: {undefended_corr_cnt}')
@@ -187,32 +240,35 @@ def main():
 
 
             # save for later analysis, currently used for biogen dataset 
-            if args.save_response:
-                os.makedirs(f'result/{args.dataset_name}',exist_ok=True)
-                if args.defense_method == 'keyword':
-                    with open(f'result/{LOG_NAME}.json','w') as f:
-                        json.dump(response_list,f,indent=4)
-                else:
-                    with open(f'result/{LOG_NAME}.json','w') as f:
-                        json.dump(response_list,f,indent=4)
+            #if args.save_response:
+            #    os.makedirs(f'result/{args.dataset_name}',exist_ok=True)
+            #    if args.defense_method == 'keyword':
+            #        with open(f'result/{LOG_NAME}.json','w') as f:
+            #            json.dump(response_list,f,indent=4)
+            #    else:
+            #        with open(f'result/{LOG_NAME}.json','w') as f:
+            #            json.dump(response_list,f,indent=4)
 
 
             if args.use_cache:
                 llm.dump_cache()
 
-
-            results_all.append({
+            result_current = {
                 "gamma": gamma,
                 "rank": i,
                 "undefended_acc": undefended_corr_cnt / (len(data_tool.data) * args.rep),
                 "defended_acc": defended_corr_cnt / (len(data_tool.data) * args.rep),
                 "undefended_asr": undefended_asr_cnt / len(data_tool.data),
                 "defended_asr": defended_asr_cnt / (len(data_tool.data) * args.rep),
-            })
-    
-    df = pd.DataFrame(results_all)
-    df.to_csv(f"./output/{LOG_NAME}.csv", index=False)
-
+                "undefended_input_tokens": undefended_input_tokens/args.rep,
+                "undefended_output_tokens": undefended_output_tokens/args.rep,
+                "defended_input_tokens": defended_input_tokens/args.rep,
+                "defended_output_tokens": defended_output_tokens/args.rep,
+                "undefended_total_time_sec": round(undefended_total_time/args.rep, 2),
+                "defended_total_time_sec": round(defended_total_time/args.rep, 2),
+            }
+            df = pd.DataFrame([result_current])
+            df.to_csv(output_csv_file, mode='a', header=False, index=False)
 
 if __name__ == '__main__':
     main()
